8b1daf979f8ff6d900b073de97b53331934a1c5f
JhonCode <jhon.380172793@ucaldas.edu.co>
2023-04-16 19:51:42 -0500

    create migration, controler and install vuejs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+import { createApp } from 'vue';
+
+//import TaskList from './components/TaskList';
+
+import App from './App.vue';
+
+createApp(App).mount('#app');
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <div id="app">
+        Hello world
+        <task-list></task-list>
+    </div>
+
+    <script src="{{ mix('js/app.js') }}"></script>
+</div>
